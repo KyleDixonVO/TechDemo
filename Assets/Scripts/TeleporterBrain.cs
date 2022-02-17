736b3a0de5694edefd53c5fc9695a27e16f1a7eb
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2022-02-17 18:11:39 -0400

    Added Portals

---
+++ Assets/Scripts/TeleporterBrain.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterBrain : MonoBehaviour
+{
+    public Transform otherPortal;
+    private Vector3 warpPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.position.z > this.transform.position.z)
+        {
+            warpPos = new Vector3(otherPortal.position.x, other.gameObject.transform.position.y, otherPortal.position.z - 1);
+        }
+        else
+        {
+            warpPos = new Vector3(otherPortal.position.x, other.gameObject.transform.position.y, otherPortal.position.z + 1);
+        }
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<CharacterController>().enabled = false;
+            other.gameObject.transform.SetPositionAndRotation(warpPos, other.gameObject.transform.rotation);
+            other.gameObject.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+}
